be7461ba9da2c5a13e1843be7b4a4829cdb54f68
Eridanus Sora <sora@sound.moe>
2019-08-28 11:55:10 +0800

    use nvencc to generate scg video

--- encoding/一图流/scg.py
+++ encoding/一图流/scg.py
@@ -18,9 +18,22 @@ else:
 # call scg
 print('生成图片')
 call('./scg_v0.0.1/static-cover-generator.exe -i "{}" -o "{}"'.format(image_path, image_path + '.output.png'))
+# AVS Template
+avs_template = r"""
+audio = DirectShowSource("{}")
+video = ImageSource("{}", fps=30, start=1, end=ceil(30*AudioLengthF(audio)/AudioRate(audio)))
+return video
+""".format(audio_path, image_path + '.output.png')
+f = open(image_path + '.temp.avs', 'wb')
+f.write(avs_template.encode('gbk'))
+f.close()
 # call ffmpeg
 print('开始压制')
 # ↓ 修改到你的 ffmpeg 路径
-call('E:/Temp/ffmpeg.exe -loop 1 -r 24 -i "{}" -i "{}" -bsf:a aac_adtstoasc -c:v libx264 -preset medium -tune stillimage -crf 18 -c:a aac -b:a 320k -shortest -pix_fmt yuv420p "{}"'.format(image_path + '.output.png', audio_path, audio_path + '.scg.mp4'))
+call('D:/Downloads/Compressed/NVEnc_4.46/NVEncC/x86/NVEncC.exe -i "{}" -o "{}"'.format(image_path + '.temp.avs', audio_path + '.scg.mp4'))
+print('开始混流')
+call('E:/Temp/ffmpeg.exe -i "{}" -i "{}" -c:v copy -c:a aac -b:a 320k "{}"'.format(audio_path + '.scg.mp4', audio_path, audio_path + '.scg.mux.mp4'))
 print('删除中间文件')
 os.remove(image_path + '.output.png')
+os.remove(image_path + '.temp.avs')
+os.remove(audio_path + '.scg.mp4')
